2588433c311ea553f92f5f8e8033ce3d9c2b5d8d
HarishSRM2025 <harishsrm25@gmail.com>
2025-12-11 16:21:15 +0530

    env

--- sequelize.js
+++ sequelize.js
@@ -1,12 +1,37 @@
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("rydmate", "root", "2526", {
+//     host: "localhost",
+//     dialect: "mysql"
+// });
+
+// sequelize.authenticate()
+//     .then(() => console.log("MySQL Connected"))
+//     .catch(err => console.log("DB Error:", err));
+
+// module.exports = sequelize;
+
+
+
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("rydmate", "root", "2526", {
-    host: "localhost",
-    dialect: "mysql"
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: { require: true, rejectUnauthorized: false }
+        }
+    }
+);
 
 sequelize.authenticate()
-    .then(() => console.log("MySQL Connected"))
+    .then(() => console.log("PostgreSQL Connected"))
     .catch(err => console.log("DB Error:", err));
 
 module.exports = sequelize;
