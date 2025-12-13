e89c70f9baf497cb09aa0954baf58003748ead3d
HarishSRM2025 <harishsrm25@gmail.com>
2025-12-13 11:19:25 +0530

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { userSignup, userSignin, getUserDetails, changePassword } = require("../controllers/userController");
+
+const {
+  userSignup,
+  userSignin,
+  getUserDetails,
+  updateUser,
+  changePassword
+} = require("../controllers/userController");
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
-router.get("/getuser/:userId",getUserDetails)
+router.get("/getuser/:userId", getUserDetails);
 router.put("/update/:userId", updateUser);
-router.put("/change-password/:userId",changePassword);
+router.put("/change-password/:userId", changePassword);
 
 module.exports = router;
