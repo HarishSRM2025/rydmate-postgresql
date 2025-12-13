1a1318fd51b145823cba4614cb381ba65a64157c
HarishSRM2025 <harishsrm25@gmail.com>
2025-12-13 10:15:44 +0530

    Update user Details

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,3 +55,41 @@ exports.getUserDetails = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.updateUser = async (req, res) => {
+  const { userId } = req.params;
+  const { name, email, phone } = req.body;
+
+  try {
+    await User.update(
+      { name, email, phone },
+      { where: { id: userId } }
+    );
+
+    res.json({ message: "Profile updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Profile update failed" });
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  const { userId } = req.params;
+  const { oldPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch)
+      return res.status(400).json({ error: "Old password is incorrect" });
+
+    const hashed = await bcrypt.hash(newPassword, 10);
+    user.password = hashed;
+    await user.save();
+
+    res.json({ message: "Password changed successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Password update failed" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { userSignup, userSignin, getUserDetails } = require("../controllers/userController");
+const { userSignup, userSignin, getUserDetails, changePassword } = require("../controllers/userController");
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
 router.get("/getuser/:userId",getUserDetails)
+router.put("/update/:userId", updateUser);
+router.put("/change-password/:userId",changePassword);
 
 module.exports = router;
